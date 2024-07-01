7d9e032823ab0f07f3d034085e04c3dba9edf1aa
Roman Cherepanov <romach007@gmail.com>
2024-07-01 17:45:49 +0300

    Update test

--- src/test/java/org/example/BookTests.java
+++ src/test/java/org/example/BookTests.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -15,13 +14,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class BookTests {
 
     @Autowired
-    private ApplicationContext context;
+    private Book book;
 
     @Test
     @DisplayName("Fetch the book bean from the context")
     public void fetchBookBean() {
-        Book book = context.getBean("book", Book.class);
+        String actualTitle = book.getTitle();
+        String expectedTitle = "One Hundred Years of Solitude";
 
-        assertEquals(book.getTitle(), "One Hundred Years of Solitude");
+        assertEquals(actualTitle, expectedTitle);
     }
 }
